13eec495ddc1467ea2ff98e7fb0b2b4a0d0bcbe4
Gerellz <70752586+Gerellz@users.noreply.github.com>
2022-02-08 19:01:12 -0600

    Create simplesim.h

---
+++ 241projects/stack3/simplesim.h
@@ -0,0 +1,24 @@
+#ifndef SIMPLESIM_H
+#define SIMPLESIM_H
+
+class simplesim
+{
+private:
+
+	int memory[100] {7777};
+
+	int accumulator = 0;
+	int instruction_counter = 0;
+	int instruction_register = 0;
+	int operation_code = 0;
+	int operand = 0;
+
+public:
+
+	simplesim();
+	bool load_program();
+	void execute_program();
+	void dump() const;
+};
+
+#endif /* SIMPLESIM_H */
