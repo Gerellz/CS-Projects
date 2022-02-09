0258fa121b0a70248fe91af5a2f61b41b170b235
Gerellz <70752586+Gerellz@users.noreply.github.com>
2022-02-08 18:55:16 -0600

    Create eval.h

---
+++ 241projects/stack2/eval.h
@@ -0,0 +1,18 @@
+#ifndef EVAL_H
+#define EVAL_H
+
+//***************************************
+//
+// eval.h
+//
+// CSCI 241 Assignment 8
+//
+// Written by Gerald Ellsworth (z1885378)
+//
+//**************************************/
+
+#include <string>
+
+int evaluate(const std::string&);
+
+#endif /* EVAL_H */
