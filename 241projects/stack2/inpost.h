b0934e3175ab6cca79b9923e17fa84be94abd1c9
Gerellz <70752586+Gerellz@users.noreply.github.com>
2022-02-08 18:55:43 -0600

    Create inpost.h

---
+++ 241projects/stack2/inpost.h
@@ -0,0 +1,20 @@
+#ifndef INPOST_H
+#define INPOST_H
+
+//***************************************
+//
+// inpost.h
+//
+// CSCI 241 Assignment 8
+//
+// Written by Gerald Ellsworth (z1885378)
+//
+//**************************************/
+
+#include <string>
+
+std::string convert(const std::string&);
+
+int precedence(char);
+
+#endif /* INPOST_H */
