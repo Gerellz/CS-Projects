aa813663a3607b58b43a6a048566df290e8a59c3
Gerellz <70752586+Gerellz@users.noreply.github.com>
2022-02-08 18:59:23 -0600

    Create inpost.h

---
+++ 241projects/stack3/inpost.h
@@ -0,0 +1,15 @@
+#ifndef INPOST_H
+#define INPOST_H
+
+//***************************************
+// Written by Gerald Ellsworth (z1885378)
+//**************************************/
+
+#include <string>
+#include <iostream>
+
+std::string convert(const std::string&);
+
+int precedence(char);
+
+#endif /* INPOST_H */
